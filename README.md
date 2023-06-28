c593135ec83ad3886b7a2069bd34e42933aba18c
Billal GHILAS <bilal@evercam.io>
2023-06-28 13:33:16 +0100

    Add encoder based on x265

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 [![Hex.pm](https://img.shields.io/hexpm/v/membrane_h265_ffmpeg_plugin.svg)](https://hex.pm/packages/membrane_h265_ffmpeg_plugin)
 [![API Docs](https://img.shields.io/badge/api-docs-yellow.svg?style=flat)](https://hexdocs.pm/membrane_h265_ffmpeg_plugin/)
 
-This package provides H264 video decoder and encoder, based on [ffmpeg](https://www.ffmpeg.org).
+This package provides H265 video decoder and encoder, based on [ffmpeg](https://www.ffmpeg.org).
 
 Documentation is available at [HexDocs](https://hexdocs.pm/membrane_h265_ffmpeg_plugin/)
 
---
+++ c_src/membrane_h265_ffmpeg_plugin/encoder.spec.exs
@@ -0,0 +1,27 @@
+module Membrane.H265.FFmpeg.Encoder.Native
+
+state_type "State"
+
+spec create(
+       width :: int,
+       height :: int,
+       pix_fmt :: atom,
+       preset :: atom,
+       tune :: atom,
+       profile :: atom,
+       max_b_frames :: int,
+       gop_size :: int,
+       framerate_num :: int,
+       framerate_denom :: int,
+       crf :: int
+     ) :: {:ok :: label, state} | {:error :: label, reason :: atom}
+
+spec encode(payload, pts :: int64, use_shm :: bool, state) ::
+      {:ok :: label, dts_list :: [int64], pts_list :: [int64], [payload]}
+  |      {:error :: label, reason :: atom}
+
+spec flush(use_shm :: bool, state) ::
+       {:ok :: label, dts_list :: [int64], pts_list :: [int64], frames :: [payload]}
+       | {:error :: label, reason :: atom}
+
+dirty :cpu, flush: 1, encode: 3
